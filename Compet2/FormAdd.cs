41f646e7241be36125567242e12e18a77635d723
NopXx <97354576+NopXx@users.noreply.github.com>
2023-02-13 23:30:38 +0700

    add alert if input data entry

--- Compet2/FormAdd.cs
+++ Compet2/FormAdd.cs
@@ -31,31 +31,39 @@ namespace Compet2
 
         private void button1_Click(object sender, EventArgs e)
         {
-            DataSet ds = new DataSet();
             string user = textBox_user.Text;
             string pass = textBox_pass.Text;
             string name = textBox_name.Text;
-            string phone = textBox_phone.Text;
-            // checked user
-            string sql_check = "select * from tb_member where m_user = '" + user + "'";
-            MySqlDataAdapter da = new MySqlDataAdapter(sql_check, db.conn);
-            da.Fill(ds, "dtMember");
-            if (ds.Tables["dtMember"].Rows.Count > 0)
+            if (user.Length == 0 || pass.Length == 0 || name.Length == 0)
             {
-                MessageBox.Show("ไม่สามารถเพิ่มข้อมูบได้ เนื่องจากมีชื่อผู้ใช้แล้ว");
-            } else
+                MessageBox.Show("กรุณาป้อนข้อมูล ผู้ใช้ รหัสผ่าน และ ชื่อ-นามสกุล");
+            }
+            else
             {
-                string sql_insert = "INSERT INTO tb_member (m_user, m_pass, m_name, m_phone) " +
-                    "VALUES ('" + user + "', '" + pass + "', '" + name + "', '" + phone + "')";
-                MySqlDataAdapter da1 = new MySqlDataAdapter(sql_insert, db.conn);
-                da1.Fill(ds, "dtInsert");
-                if (ds.Tables["dtInsert"] == null)
+                DataSet ds = new DataSet();
+                string phone = textBox_phone.Text;
+                // checked user
+                string sql_check = "select * from tb_member where m_user = '" + user + "'";
+                MySqlDataAdapter da = new MySqlDataAdapter(sql_check, db.conn);
+                da.Fill(ds, "dtMember");
+                if (ds.Tables["dtMember"].Rows.Count > 0)
                 {
-                    MessageBox.Show("ทำรายการสำเร็จ");
-                    textBox_user.Text = "";
-                    textBox_pass.Text = "";
-                    textBox_name.Text = "";
-                    textBox_phone.Text = "";
+                    MessageBox.Show("ไม่สามารถเพิ่มข้อมูบได้ เนื่องจากมีชื่อผู้ใช้แล้ว");
+                }
+                else
+                {
+                    string sql_insert = "INSERT INTO tb_member (m_user, m_pass, m_name, m_phone) " +
+                        "VALUES ('" + user + "', '" + pass + "', '" + name + "', '" + phone + "')";
+                    MySqlDataAdapter da1 = new MySqlDataAdapter(sql_insert, db.conn);
+                    da1.Fill(ds, "dtInsert");
+                    if (ds.Tables["dtInsert"] == null)
+                    {
+                        MessageBox.Show("ทำรายการสำเร็จ");
+                        textBox_user.Text = "";
+                        textBox_pass.Text = "";
+                        textBox_name.Text = "";
+                        textBox_phone.Text = "";
+                    }
                 }
             }
 
